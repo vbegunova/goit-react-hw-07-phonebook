4c25586efa9ce3626a2db1289e2bea854abb2fe3
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-08-22 12:45:27 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,12 @@
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
-import { useSelector } from 'react-redux';
-import { getError, getIsLoading } from 'redux/selectors';
 
 export const App = () => {
-  const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);
-
   return (
     <>
       <h1>Phonebook</h1>
-      <ContactForm />      
+      <ContactForm />
       <h2>Contacts</h2>
       <Filter />
       <ContactList />
